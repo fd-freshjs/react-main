5713392f65eeddc4e4a41d5bd5ae69d213d34218
Yevhenii Lapshynov <fd-freshjs>
2022-05-12 15:39:01 +0300

    Add context usage

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import HomePage from './pages/Home';
 import ProfilePage from './pages/Profile';
 import DocsPage from './pages/DocsPage';
 import './App.scss';
+import { LangContext, UserContext } from './contexts';
 
 const LoginPage = lazy(() => import('./pages/Login'));
 const Counter = lazy(() => import('./pages/Counter'));
@@ -17,72 +18,88 @@ class App extends Component {
     super(props);
 
     this.state = {
-      user: null,
+      user: { firstName: 'Ihor' },
+      lang: 'en',
     };
   }
 
+  setUser = () => {
+    this.setState({ user: { firstName: Math.random() * 10 } });
+  }
+
+  setLang = () => {
+    this.setState((state) => ({ lang: state.lang === 'ru' ? 'en' : 'ru' }));
+  }
+
   render () {
+    const { user, lang } = this.state;
+
     return (
       <BrowserRouter>
-        <header>
-          <Link to='/'>
-            <img src='/logo192.png' alt='logo' width='50' />
-          </Link>
+        <button onClick={this.setLang}>Change Lang</button>
+        <button onClick={this.setUser}>Change User</button>
+        <LangContext.Provider value={lang}>
+          <UserContext.Provider value={user}>
+            <header>
+              <Link to='/'>
+                <img src='/logo192.png' alt='logo' width='50' />
+              </Link>
 
-          <nav>
-            <ul>
-              <li>
-                {this.state.user ? (
-                  <Link to='/profile'>Profile</Link>
-                ) : (
-                  <Link to='/login'>Login</Link>
-                )}
-              </li>
-              <li>
-                <Link to='/calc'>Calculator</Link>
-              </li>
-              <li>
-                <Link to='/counter'>Counter</Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
+              <nav>
+                <ul>
+                  <li>
+                    {this.state.user ? (
+                      <Link to='/profile'>Profile</Link>
+                    ) : (
+                      <Link to='/login'>Login</Link>
+                    )}
+                  </li>
+                  <li>
+                    <Link to='/calc'>Calculator</Link>
+                  </li>
+                  <li>
+                    <Link to='/counter'>Counter</Link>
+                  </li>
+                </ul>
+              </nav>
+            </header>
 
-        
-          <main id="main">
-            <Switch>
-              <Route exact path='/' component={HomePage} />
+            <main id="main">
+              <Switch>
+                <Route exact path='/' component={HomePage} />
 
-              <Route exact path='/docs'>
-                {props => {
-                  return <DocsPage />;
-                }}
-              </Route>
+                <Route exact path='/docs'>
+                  {props => {
+                    return <DocsPage />;
+                  }}
+                </Route>
 
-              <Route exact path='/counter'>
-                <Counter />
-              </Route>
+                <Route exact path='/counter'>
+                  <Counter />
+                </Route>
 
-              <Route exact path='/calc'>
-                <Calculator scale='km' />
-              </Route>
+                <Route exact path='/calc'>
+                  <Calculator scale='km' />
+                </Route>
 
-              <Route exact path='/login'>
-                <Suspense fallback={<CircularProgress />}>
-                  <LoginPage />
-                </Suspense>
-              </Route>
+                <Route exact path='/login'>
+                  <Suspense fallback={<CircularProgress />}>
+                    <LoginPage />
+                  </Suspense>
+                </Route>
 
-              <PrivateRoute
-                route={{ exact: true, path: '/profile', component: ProfilePage }}
-                auth={this.state.user}
-              />
+                <PrivateRoute
+                  route={{ exact: true, path: '/profile', component: ProfilePage }}
+                  auth={this.state.user}
+                />
 
-              <Route path='*'>
-                <Redirect to='/' />
-              </Route>
-            </Switch>
-          </main>
+                <Route path='*'>
+                  <Redirect to='/' />
+                </Route>
+              </Switch>
+            </main>
+          </UserContext.Provider>
+        </LangContext.Provider>
       </BrowserRouter>
     );
   }
--- src/pages/Home/WelcomeSection/index.jsx
+++ src/pages/Home/WelcomeSection/index.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { UserContext, LangContext } from '../../../contexts';
 import './WelcomeSection.css';
 
+const text = {
+  en: (user) => `Welcome, ${user ? user.firstName : 'Stranger'}`,
+  ru: (user) => `Добро пожаловать, ${user ? user.firstName : 'Незнакомец'}`,
+}
+
 function WelcomeSection() {
+
   return (
-    <section className="welcomeSection">
-        <h1 className="mainHeading">React</h1>
-        <p className="mainDescription">JavaScript-библиотека для создания пользовательских интерфейсов</p>
-        <div className="mainLinks">
-            <Link className="linkButton" to="/docs">Начать работу</Link>
-            <a className="link" href="https://ru.reactjs.org/tutorial/tutorial.html">Перейти к введению {'>'}</a>
-        </div>
-    </section>
-  )
+    <LangContext.Consumer>
+      {(lang) =>
+        <UserContext.Consumer>
+          {(user = {}) =>
+            (<section className="welcomeSection">
+              <h1 className="mainHeading">{text[lang](user)}</h1>
+              <p className="mainDescription">JavaScript-библиотека для создания пользовательских интерфейсов</p>
+              <div className="mainLinks">
+                  <Link className="linkButton" to="/docs">Начать работу</Link>
+                  <a className="link" href="https://ru.reactjs.org/tutorial/tutorial.html">Перейти к введению {'>'}</a>
+              </div>
+            </section>)
+          }
+        </UserContext.Consumer>
+      }
+    </LangContext.Consumer>
+  );
 }
 
 export default WelcomeSection
