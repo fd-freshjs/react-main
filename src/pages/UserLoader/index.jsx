96bf8ec4aa44fe38057827a14e70f05a41292319
Yevhenii Lapshynov <fd-freshjs>
2022-05-13 16:52:55 +0300

    Add page, did update, should update

--- src/pages/UserLoader/index.jsx
+++ src/pages/UserLoader/index.jsx
@@ -13,36 +13,74 @@ class UserLoader extends Component {
     };
   }
 
-  componentDidMount() {
+  componentDidMount () {
     this.loadUsers();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.page !== this.state.page) {
+      this.loadUsers();
+    }
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.page === this.state.page && nextState.users === this.state.users) {
+      return false;
+    }
+
+    return true;
   }
 
   loadUsers = async () => {
     const { page } = this.state;
 
     try {
-      this.setState((state) => ({ ...state, isLoading: true }));
-  
-      const users = await fetchUsers(page);
-      this.setState((state) => ({ ...state, users, error: null }));
+      this.setState(state => ({ ...state, isLoading: true }));
+
+      const { results: users } = await fetchUsers(page);
+      this.setState(state => ({ ...state, users, error: null }));
     } catch (error) {
-      this.setState((state) => ({ ...state, error }));
+      this.setState(state => ({ ...state, error }));
     } finally {
-      this.setState((state) => ({ ...state, isLoading: false }));
+      this.setState(state => ({ ...state, isLoading: false }));
     }
-  }
+  };
+
+  loadPage = e => {
+    const maxPage = 5;
+
+    this.setState(state => {
+      const newPage = e.target.name === 'next'
+        ? Math.min(state.page + 1, maxPage)
+        : Math.max(state.page - 1, 1);
+
+      return ({
+        ...state,
+        page: newPage,
+      });
+    });
+  };
 
   render () {
-    const { isLoading, error, users } = this.state;
-
-    if (isLoading) {
-      return <div>Загрузка...</div>;
-    }
+    console.log('render');
+    const { isLoading, error, users, page } = this.state;
 
     return (
       <div>
+        {isLoading && <div>Загрузка...</div>}
         <button onClick={this.loadUsers}>Загрузить новых пользователей</button>
+
+        <div>
+          <button name='prev' onClick={this.loadPage}>
+            {'<'}
+          </button>
+          {page}
+          <button name='next' onClick={this.loadPage}>
+            {'>'}
+          </button>
+        </div>
         {error && <div>Error: {error.toString()}</div>}
+
         {users.map(u => {
           return (
             <div key={u.login.uuid}>
